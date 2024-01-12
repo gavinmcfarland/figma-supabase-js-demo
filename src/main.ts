33f58fc7086fbb7a9de49d15c790b773405511ea
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2024-01-12 21:46:13 +0000

    Work in progress

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,31 @@
 export default function () {
-	figma.showUI(__html__, { width: 300, height: 500, themeColors: true });
+	figma.showUI(__html__, { width: 300, height: 500, themeColors: true })
 
 	figma.ui.onmessage = async (msg) => {
 		// Required for supabase.js
 
-		if (msg.type === "set-client-storage") {
-			console.log("set-client-storage");
+		if (msg.type === 'set-client-storage') {
+			console.log('set-client-storage')
 			figma.clientStorage.setAsync(msg.key, msg.value).then(() => {
-				figma.ui.postMessage({ type: "client-storage-set" });
-			});
+				figma.ui.postMessage({ type: 'client-storage-set' })
+			})
 		}
 
-		if (msg.type === "remove-client-storage") {
-			console.log("remove-client-storage");
+		if (msg.type === 'remove-client-storage') {
+			console.log('remove-client-storage')
 			figma.clientStorage.deleteAsync(msg.key).then(() => {
-				figma.ui.postMessage({ type: "client-storage-removed" });
-			});
+				figma.ui.postMessage({ type: 'client-storage-removed' })
+			})
 		}
 
-		if (msg.type === "get-client-storage") {
+		if (msg.type === 'get-client-storage') {
 			figma.clientStorage.getAsync(msg.key).then((res) => {
 				figma.ui.postMessage({
-					type: "post-client-storage",
+					type: 'post-client-storage',
 					key: msg.key,
 					value: res,
-				});
-			});
+				})
+			})
 		}
-	};
+	}
 }
