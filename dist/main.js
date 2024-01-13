d0a4d4bfe627b4387375c99f7ec4c90b35d9909b
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2024-01-13 16:24:40 +0000

    Moved files around

--- dist/main.js
+++ dist/main.js
@@ -1,49 +1,50 @@
-// node_modules/plugma/frameworks/common/main/saveFigmaStyles.ts
+// ../plugma/packages/plugma/frameworks/common/main/saveFigmaStyles.ts
 function saveFigmaStyles() {
-	if (true) {
-		figma.ui.on("message", async (msg) => {
-			if (msg.event === "save-figma-stylesheet") {
-				figma.clientStorage.setAsync("figma-stylesheet", msg.styles);
-			}
-			if (msg.event === "get-figma-stylesheet") {
-				let styles = await figma.clientStorage.getAsync("figma-stylesheet");
-				figma.ui.postMessage({
-					event: "pass-figma-stylesheet",
-					styles
-				});
-			}
-		});
-	}
+  if (true) {
+    figma.ui.on("message", async (msg) => {
+      if (msg.event === "save-figma-stylesheet") {
+        figma.clientStorage.setAsync("figma-stylesheet", msg.styles);
+      }
+      if (msg.event === "get-figma-stylesheet") {
+        let styles = await figma.clientStorage.getAsync("figma-stylesheet");
+        figma.ui.postMessage({
+          event: "pass-figma-stylesheet",
+          styles
+        });
+      }
+    });
+  }
 }
 
 // src/main.ts
+console.clear();
 function main_default() {
-	figma.showUI(__html__, { width: 300, height: 500, themeColors: true });
-	figma.ui.onmessage = async (msg) => {
-		if (msg.type === "set-client-storage") {
-			console.log("set-client-storage");
-			figma.clientStorage.setAsync(msg.key, msg.value).then(() => {
-				figma.ui.postMessage({ type: "client-storage-set" });
-			});
-		}
-		if (msg.type === "remove-client-storage") {
-			console.log("remove-client-storage");
-			figma.clientStorage.deleteAsync(msg.key).then(() => {
-				figma.ui.postMessage({ type: "client-storage-removed" });
-			});
-		}
-		if (msg.type === "get-client-storage") {
-			figma.clientStorage.getAsync(msg.key).then((res) => {
-				figma.ui.postMessage({
-					type: "post-client-storage",
-					key: msg.key,
-					value: res
-				});
-			});
-		}
-	};
+  figma.showUI(__html__, { width: 300, height: 500, themeColors: true });
+  figma.ui.onmessage = async (msg) => {
+    if (msg.type === "set-client-storage") {
+      console.log("set-client-storage");
+      figma.clientStorage.setAsync(msg.key, msg.value).then(() => {
+        figma.ui.postMessage({ type: "client-storage-set" });
+      });
+    }
+    if (msg.type === "remove-client-storage") {
+      console.log("remove-client-storage");
+      figma.clientStorage.deleteAsync(msg.key).then(() => {
+        figma.ui.postMessage({ type: "client-storage-removed" });
+      });
+    }
+    if (msg.type === "get-client-storage") {
+      figma.clientStorage.getAsync(msg.key).then((res) => {
+        figma.ui.postMessage({
+          type: "post-client-storage",
+          key: msg.key,
+          value: res
+        });
+      });
+    }
+  };
 }
 
-// ../../../../private/var/folders/wr/55c039r173z6tm1c23b89d7r0000gn/T/temp_1705082561038.js
+// ../../../../private/var/folders/wr/55c039r173z6tm1c23b89d7r0000gn/T/temp_1705163046824.js
 saveFigmaStyles();
 main_default();
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+console.clear()
+
 export default function () {
 	figma.showUI(__html__, { width: 300, height: 500, themeColors: true })
 
